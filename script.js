9cad70fbe456757ddb89c618fa8ae1c510bf9848
hambiraki <hambiraki@outlook.com>
2024-09-24 00:06:02 +0900

    表と配列を同期

--- script.js
+++ script.js
@@ -55,3 +55,85 @@ function handleImageUpload(event) {
 }
 
 
+// プレイヤーデータの定数
+const bluePlayers = [
+    { name: 'プレイヤー1', result: '〇勝敗' },
+    { name: 'プレイヤー2', result: '〇勝敗' },
+    { name: 'プレイヤー3', result: '〇勝敗' },
+    { name: 'プレイヤー4', result: '〇勝敗' },
+    { name: 'プレイヤー5', result: '〇勝敗' },
+    { name: 'プレイヤー6', result: '〇勝敗' },
+    { name: 'プレイヤー7', result: '〇勝敗' },
+    { name: 'プレイヤー8', result: '〇勝敗' },
+    { name: 'プレイヤー9', result: '〇勝敗' },
+    { name: 'プレイヤー10', result: '〇勝敗' },
+    { name: '', result: '' },
+    { name: '', result: '' },
+];
+// プレイヤーデータの定数
+const redPlayers = [
+    { name: 'プレイヤー1', result: '×勝敗' },
+    { name: 'プレイヤー2', result: '×勝敗' },
+    { name: 'プレイヤー3', result: '×勝敗' },
+    { name: 'プレイヤー4', result: '×勝敗' },
+    { name: 'プレイヤー5', result: '×勝敗' },
+    { name: 'プレイヤー6', result: '×勝敗' },
+    { name: 'プレイヤー7', result: '×勝敗' },
+    { name: 'プレイヤー8', result: '×勝敗' },
+    { name: 'プレイヤー9', result: '×勝敗' },
+    { name: 'プレイヤー10', result: '×勝敗' },
+    { name: '', result: '' },
+    { name: '', result: '' },
+];
+
+
+// 画像を読み取る
+function readScoreImage() {
+
+
+    // 表に反映
+    function showScoreTable(players,nameInputs,resultInputs) {
+        players.forEach((player, index) => {
+            if (nameInputs[index] && resultInputs[index]) {
+                nameInputs[index].value = player.name;      // 名前を入力
+                resultInputs[index].value = player.result;  // 結果を入力
+            }
+        });
+    }
+    showScoreTable(
+        bluePlayers,
+        document.querySelectorAll('.blue-table tbody tr td:nth-child(1) input'),
+        document.querySelectorAll('.blue-table tbody tr td:nth-child(2) input'),    
+    );
+    showScoreTable(
+        redPlayers,
+        document.querySelectorAll('.red-table tbody tr td:nth-child(1) input'),
+        document.querySelectorAll('.red-table tbody tr td:nth-child(2) input'),    
+    );
+}
+
+
+
+function updateScoreTables(){
+    function updateScoreTable(players,nameInputs,resultInputs){
+        players.forEach((player, index) => {
+            if (nameInputs[index] && resultInputs[index]) {
+                player.name = nameInputs[index].value;      // 名前を入力
+                player.result = resultInputs[index].value;  // 結果を入力
+            }
+        });
+    }
+    updateScoreTable(
+        bluePlayers,
+        document.querySelectorAll('.blue-table tbody tr td:nth-child(1) input'),
+        document.querySelectorAll('.blue-table tbody tr td:nth-child(2) input'),    
+
+    );
+    updateScoreTable(
+        redPlayers,
+        document.querySelectorAll('.red-table tbody tr td:nth-child(1) input'),
+        document.querySelectorAll('.red-table tbody tr td:nth-child(2) input'),    
+    );
+    console.log(bluePlayers);
+}
+
