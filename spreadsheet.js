bb01b4e1ba36398f7e606b9df5ac504183577555
hambiraki <hambiraki@outlook.com>
2024-10-18 00:28:23 +0900

    昨日

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,12 +1,69 @@
 /**
  * 
  */
+const CLIENT_ID = "439336057984-01tr2f1abg2n349bkt4035sj4jlg6lg7.apps.googleusercontent.com";
+const SCOPE = 'https://www.googleapis.com/auth/spreadsheets'; // スプレッドシートへのアクセスを許可するスコープ
+const AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
+const TOKEN_URL = 'https://oauth2.googleapis.com/token';
+
+function login() {
+    const authUrl = `${AUTH_URL}`
+     + `?client_id=${CLIENT_ID}`
+     + `&redirect_uri=${encodeURIComponent(window.location.origin)}` 
+     + `&response_type=token`
+     + `&scope=${encodeURIComponent(SCOPE)}`;
+    //  + `&access_type=offline`;
+     window.location.href = authUrl;
+}
+
+function redirectToGoogleOAuth() {
+    const authUrl = `${AUTH_URL}`
+     + `?client_id=${CLIENT_ID}`
+     + `&redirect_uri=${encodeURIComponent(window.location.origin)}` 
+     + `&response_type=token`
+     + `&scope=${encodeURIComponent(SCOPE)}`;
+    //  + `&access_type=offline`;
+    window.open(authUrl, "_blank");
+}
+
+function getAccessToken() {
+    // URLのハッシュからアクセストークンを取得
+    const hash = window.location.hash;
+    if (hash) {
+        const params = new URLSearchParams(hash.substring(1)); // '#' を取り除く
+        return accessToken = params.get('access_token');
+    }
+    return undefined;
+}
+
+// 4. 認証コードをキャプチャしてアクセストークンを取得
+async function handleAuthCallback() {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+    if (code) {
+        try {
+            const accessToken = await getAccessToken(code);
+            console.log('Access Token:', accessToken);
+            // アクセストークンを使ってGAS APIなどを呼び出す
+        } catch (error) {
+            console.error('Error during authentication:', error);
+        }
+    }
+}
+
+// ページが読み込まれたときに、認証コードがあれば処理する
+window.onload = () => {
+    handleAuthCallback();
+};
 
 function sendScore(){
     // GASのURLを指定
-    const deployId = "AKfycbxVVLkxiQzTKUOiKyC8q8uPsGX1CcWVLDWchTJ744PHHc9T-GYaoIqVRohwcCGGY_xAYQ";
-    const gasUrl = `https://script.google.com/macros/s/${deployId}/exec`;
-    
+    const deployId = "AKfycbxZq_kdOldm5gkCe_H3JgLyxSigtmwoKbscHCy_lu4ehwkA_CRaQjbpnXuKDxq9rkSC";
+    const gasUrl = `https://script.googleapis.com/v1/scripts/AKfycbzR7x9QkrHpk8zvFBE7dnK_aDqSt5IV7-i4t9ZU4RzchCPu_3rakL0jQ5iaGgXW56Va_A:run`;
+
+    const access_token = getAccessToken();
+    console.log(access_token);
+
     // POSTするデータを設定
     const postData = {
         spreadsheetUrl: "https://docs.google.com/spreadsheets/d/180GM9rGzcetAT_s_qkwSwetYZPIpFQyKb7gHl8Q8Cy0/edit?gid=0#gid=0",
@@ -17,9 +74,10 @@ function sendScore(){
     const requestOptions = {
         method: "POST",
         headers: {
-        "Content-Type": "application/json"
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(postData)
+        body: postData
     };
     
     // fetch APIを使ってリクエストを送信
@@ -32,6 +90,25 @@ function sendScore(){
             console.error("エラーが発生しました:", error);
         });
 }
+
+// function authenticate() {
+//     return gapi.auth2.getAuthInstance()
+//         .signIn({scope: "https://www.googleapis.com/auth/spreadsheets"})
+//         .then(() => console.log("Sign-in successful"))
+//         .catch(err => console.error("Error signing in", err));
+// }
+
+// function loadClient() {
+//     gapi.client.setApiKey("YOUR_API_KEY");
+//     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/drive/v3/rest")
+//         .then(() => console.log("GAPI client loaded for API"))
+//         .catch(err => console.error("Error loading GAPI client for API", err));
+// }
+
+// gapi.load("client:auth2", () => {
+//     gapi.auth2.init({client_id: clientId});
+// });
+
 
 
 // 名前:勝敗(MVP)に変形する
